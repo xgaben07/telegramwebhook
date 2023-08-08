8807a5351b5920267224f2b94f3db45426efd7e1
xgaben07 <87758773+xgaben07@users.noreply.github.com>
2023-08-08 08:44:19 +0800

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-urls = dict(tvcoins="https://www.tradingview.com/tvcoins/details/",
-            signin="https://www.tradingview.com/accounts/signin/",
-            tvchart="https://www.tradingview.com/chart/")
